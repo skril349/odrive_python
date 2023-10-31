73e31d41162d756685580b31f380a95b0c3e1ad0
skril349 <tvives.cabaleiro@gmail.com>
2023-10-31 13:17:45 +0100

    solving bug

--- PythonOdrive/main.py
+++ PythonOdrive/main.py
@@ -10,7 +10,7 @@ import datetime
 # Configuración de ODrive y MQTT
 my_drive = setup_odrive()
 client = setup_mqtt()
-plot_or_not = True
+plot_or_not = False
 # Mantén el bucle hasta que se reciba un mensaje
 while get_received_message() == False:
     pass  # Espera hasta que get_received_message() sea True
@@ -24,11 +24,11 @@ positions = []
 intensities = []
 voltages = []
 torques = []
+
 # Enabling interactive mode for real-time plotting
 plt.ion()
 # Create a figure for plotting
-if plot_or_not:
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(10, 8))
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(10, 8))
 
 # Main loop for data collection and plotting
 t0 = time.monotonic()
@@ -49,19 +49,6 @@ try:
         intensities.append(intensity)
         voltages.append(voltage)
         torques.append(torque)
-     
-        data_queue.append(positions[-1])
-        if len(intensities) > max_data_points:
-            intensities.pop(0)
-            voltages.pop(0)
-            torques.pop(0)
-            positions.pop(0)
-            timestamps.pop(0)
-
-        if plot_or_not:
-            update_plot(timestamps[-max_data_points:], data_queue, intensities[-max_data_points:],
-                        voltages[-max_data_points:], torques[-max_data_points:], ax1, ax2, ax3, ax4)
-        
 
         data_to_publish={
         "timestamp":(time.monotonic() - t0),
@@ -71,6 +58,20 @@ try:
         "torque" :torque
         }
         client.publish("data", str(data_to_publish))
+     
+        data_queue.append(positions[-1])
+        if len(intensities) > max_data_points:
+            intensities.pop(0)
+            voltages.pop(0)
+            torques.pop(0)
+            positions.pop(0)
+            timestamps.pop(0)
+
+        update_plot(timestamps[-max_data_points:], data_queue, intensities[-max_data_points:],
+                        voltages[-max_data_points:], torques[-max_data_points:], ax1, ax2, ax3, ax4)
+        
+
+        
 
 
         plt.pause(0.01)
